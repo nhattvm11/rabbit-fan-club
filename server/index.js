1344bc0f91f1b52e68d44eb7390f76df4df25376
minh nhat <minhnhat11>
2019-08-06 15:18:52 +0700

    update config

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ async function start () {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(process.env.PORT || port, host)
+  app.listen(port, host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
